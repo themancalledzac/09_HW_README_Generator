3bf655d5a72dc7be59c2de1a7167ca1464225393
Zac Edens <themancalledzac@gmail.com>
2020-11-27 18:57:34 -0800

    used previous in class example to build the response. Still need to figure out how to export not as an object.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-// 
+// linking to inquirer modules 
 const inquirer = require('inquirer');
+// giving access to your file system to read/write/modify directly on the users computer.
+const fs = require('fs');
+const { NONAME } = require('dns');
+
 //
+inquirer
+    .prompt([
+        {
+            name: 'name',
+            type: 'input',
+            message: 'What is your name?',
+            default: 'none',
+
+        },
+    ])
+    .then((response) => {
+
+        const fileName = `${response.name.toLowerCase().split(" ").join("")}.md`;
+        fs.writeFile(fileName, JSON.stringify(response, null, "\t"), (err) =>
+        err ? console.log(err) : console.log("Success!")
+        );
+    });
